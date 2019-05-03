05727b5e569c6ef1ebf83f1bd06c1d9995a79d35
Erick J. Gamez Diaz <erickj.gamezdiaz@Ericks-MacBook-Pro.local>
2019-05-02 20:31:17 -0400

    your message

---
+++ script.js
@@ -0,0 +1,12 @@
+function marinaraSauce(Adam){
+    var two = 2;
+    var oneFourth = "1/4";
+    console.log("Hey, " + Adam + " This is my marinara sauce recipe.");
+    console.log("Stir in " + oneFourth + " cup roughly chopped fresh basil, salt and pepper as needed.");
+    console.log("Heat a medium-large saucepan over medium heat.");
+    console.log("Add  tspns of oil and 5 carlic cloves, cook until golden, about " + two +  " minutes");
+    console.log("Add " + two + " cup water, " + two + " cans of crushed tomates, and salt and season with black pepper to taste.");
+    console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+    console.log("Stir in " + oneFourth + " cup roughly chopped fresh basil, salt and pepper as needed.");
+}
+marinaraSauce("Adam");
